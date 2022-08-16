012c575469fe5dc9a7871a347edc4e3167fab1fa
sjpurusothaman <purushothaman.sj@janaanalysis.com>
2022-08-17 01:16:46 +0530

    Make it better

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const port = 5050;
+const port = process.env.PORT || 5050;
 const bodyParser = require('body-parser');
+const hostname = '0.0.0.0';
 require('dotenv/config');
 
 app.use(bodyParser.json());
@@ -20,8 +21,10 @@ app.get('/', (req, res) => {
 
 //Connect To DB
 mongoose.connect(
-    process.env.DB_CONNECTION, 
-    { useUnifiedTopology: true, useNewUrlParser: true }, 
+    process.env.DB_CONNECTION,
+    { useUnifiedTopology: true, useNewUrlParser: true },
     () => console.log('Connected to DB!'))
-    
-app.listen(port, () => console.log(`Server listening on the port::${port}`));
+
+app.listen(port, hostname, () => {
+    console.log(`Server listening on the port::${port}`)
+});
